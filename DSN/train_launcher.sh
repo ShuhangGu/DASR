369b05dd388b0665178312dc4e16eab4b3850382
DizzyYunxuan <516488199@qq.com>
2020-10-15 23:57:53 +0800

    debug

--- DSN/train_launcher.sh
+++
@@ -1,6 +0,0 @@
-python train.py --dataset aim2019 --artifacts tdsr \
-                --generator DeResnet --discriminator FSD --norm_layer Instance \
-                --filter wavelet --cat_or_sum cat  \
-                --batch_size 8 --num_workers 8 --crop_size 256 \
-                --save_path 0618_DeResnet+wavcat+FSD+wtex0.03
-
--- PerceptualSimilarity/scripts/eval_valsets.sh
+++
@@ -1,3 +0,0 @@
-
-python ./test_dataset_model.py --dataset_mode 2afc --model net-lin --net alex --use_gpu --batch_size 50
-
