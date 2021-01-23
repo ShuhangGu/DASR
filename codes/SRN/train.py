8c2f4a6dad42c77a76283a7fc468903921c877e9
DizzyYunxuan <516488199@qq.com>
2021-01-23 15:24:39 +0800

    update

--- codes/SRN/train.py
+++ codes/SRN/train.py
@@ -136,7 +136,8 @@ def main():
 
                     visuals = model.get_current_visuals()
                     sr_img = util.tensor2img(visuals['SR'])  # uint8
-                    gt_img = util.tensor2img(visuals['HR'])  # uint8
+                    if 'HR' in opt['datasets']['val']['mode']:
+                        gt_img = util.tensor2img(visuals['HR'])  # uint8
                     if opt['val_lpips']:
                         lpips = visuals['LPIPS']
                         avg_lpips += lpips
@@ -150,12 +151,13 @@ def main():
                     util.save_img(sr_img, save_img_path)
 
                     # calculate PSNR
-                    crop_size = opt['scale']
-                    gt_img = gt_img / 255.
-                    sr_img = sr_img / 255.
-                    cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                    cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                    avg_psnr += util.calculate_psnr(cropped_sr_img * 255, cropped_gt_img * 255)
+                    if 'HR' in opt['datasets']['val']['mode']:
+                        crop_size = opt['scale']
+                        gt_img = gt_img / 255.
+                        sr_img = sr_img / 255.
+                        cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                        cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                        avg_psnr += util.calculate_psnr(cropped_sr_img * 255, cropped_gt_img * 255)
                 avg_psnr = avg_psnr / idx
                 if opt['val_lpips']:
                     avg_lpips = avg_lpips / idx
